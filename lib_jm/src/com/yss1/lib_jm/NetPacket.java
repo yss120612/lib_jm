7441abed35ff9733f1f10f0b348e0d7d5d130471
yss120612 <yss1@yandex.ru>
2021-07-28 17:59:23 +0800

    a111

--- lib_jm/src/com/yss1/lib_jm/NetPacket.java
+++ lib_jm/src/com/yss1/lib_jm/NetPacket.java
@@ -4,15 +4,22 @@
  */
 package com.yss1.lib_jm;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  *
  * @author ys
  */
 public class NetPacket {
+
+    private ArrayList<String> arrayList;
 
     public static enum PType {
 
@@ -75,87 +82,113 @@ public class NetPacket {
                 case 'O':
                     return TORG_MAKE;
                 case 'Y':
-                    return PARTY_NEEDREPLY;    
+                    return PARTY_NEEDREPLY;
                 case 'W':
-                    return RASKLAD_RESULT;        
-                 case 'F':
-                    return PRIKUP_RESULT;           
+                    return RASKLAD_RESULT;
+                case 'F':
+                    return PRIKUP_RESULT;
 
             }
             return EMPTY;
         }
     }
-    private PType packetType;
-    private StringBuilder buffer;
-    private final Set<String> contragents = new LinkedHashSet<>();
-    private boolean multicast;
+
+    public static enum AddressType {
+        NOBODY('N'),    //никто
+        ALLNOTME('A'),  //все кроме меня
+        LEFT_USER('L'),
+        UPPER_USER('U'),
+        RIGHT_USER('R'),
+        DOWN_USER('D');
+        private char _letter;
+
+        AddressType(char mc) {
+            _letter = mc;
+        }
+
+        public char getLetter() {
+            return _letter;
+        }
+
+        public static AddressType getAddressType(char c) {
+            switch (c) {
+                case 'A':
+                    return ALLNOTME;
+                case 'L':
+                    return LEFT_USER;
+                case 'U':
+                    return UPPER_USER;
+                case 'R':
+                    return RIGHT_USER;
+                case 'D':
+                    return DOWN_USER;
+            }
+            return NOBODY;
+        }
+    }
+
+
+
+    private PType contentType;
+    private AddressType _sender;
+    private AddressType _send_to;
+    private StringBuilder data;
+
+    public AddressType get_sender() {
+        return _sender;
+    }
+
+    public AddressType get_send_to() {
+        return _send_to;
+    }
+
 
     public NetPacket() {
-        buffer = new StringBuilder();
-        packetType = PType.EMPTY;
+        data = new StringBuilder();
+        contentType = PType.EMPTY;
+        _send_to = AddressType.NOBODY;
+        _sender = AddressType.NOBODY;
     }
 
-    public boolean isMulticast() {
-        return multicast;
+    //public boolean isMulticast() {
+    //   return _send_to==RecvType.ALLNOTME;
+    //}
+
+    //public void setMulticast(boolean multicast) {
+    //    _send_to==RecvType.ALLNOTME
+    //}
+
+    public void set_sender(AddressType _sender) {
+        this._sender = _sender;
     }
 
-    public void setMulticast(boolean multicast) {
-        this.multicast = multicast;
+    public void set_send_to(AddressType _send_to) {
+        this._send_to = _send_to;
     }
 
-    public PType getPacketType() {
-        return packetType;
+    public PType getContentType() {
+        return contentType;
     }
 
-    public void setPacketType(PType packetType) {
-        this.packetType = packetType;
+    public void setContentType(PType contentType) {
+        this.contentType = contentType;
     }
 
-    public void setData(String s) {
-        buffer.setLength(0);
-        buffer.append(s);
+    public void setContent(String s) {
+        data.setLength(0);
+        data.append(s);
     }
 
-    public void setData(StringBuilder s) {
-        buffer = s;
+    public void setContent(StringBuilder s) {
+        data = s;
     }
 
-    public String getContragent() {
-        synchronized (contragents) {
-            if (!contragents.isEmpty()) {
-                Iterator<String> itr = contragents.iterator();
-                return itr.next();
-            }
-        }
-        return "";
-    }
-
-    public void clearContragents() {
-        synchronized (contragents) {
-            contragents.clear();
-        }
-    }
-
-    public void addContragent(String rcp) {
-        synchronized (contragents) {
-            contragents.add(rcp);
-        }
-    }
-
-    public void removeContragent(String rcp) {
-        synchronized (contragents) {
-            contragents.remove(rcp);
-        }
-    }
-
-    public boolean haveContragents() {
-        synchronized (contragents) {
-            return !contragents.isEmpty();
-        }
+    public void appendContent(String s) {
+        data.append(s);
     }
 
     public String getContent() {
-        return buffer.toString();
+        return data.toString();
     }
 
     /**
@@ -163,25 +196,27 @@ public class NetPacket {
      *
      * @return
      */
-    public byte[] getData() {
-        return (packetType.getLetter() + buffer.toString()).getBytes();
+    public String prepare2sendPacket() {
+        return "{CT=" + contentType.getLetter() + " ,RCPT=" + _send_to.getLetter() + " ,SENDER=" + _sender.getLetter() + " ,DATA=" + data.toString() + "}";
     }
 
     /**
      * populate NetPacket when received RAW [] bytes
      *
-     * @param bf received data
      * @return
      */
-    public String setData(byte[] bf) {
-        buffer.setLength(0);
-        buffer.append(new String(bf));
-        if (buffer.length() < 1) {
-            packetType = PType.EMPTY;
-            return "";
+    public void makePacket(String s) {
+        List<String> al = Stream.of(s.replaceAll("[\\{,\\}]", "").split("\\,=")).collect(Collectors.toList());
+        al.forEach(String::trim);
+        for (int i = 0; i < al.size(); i += 2) {
+            if (al.get(i).equals("CT") && i + 1 < al.size())
+                contentType = PType.getPT(al.get(i + 1).charAt(0));
+            else if (al.get(i).equals("RCPT") && i + 1 < al.size())
+                _send_to = AddressType.getAddressType(al.get(i + 1).charAt(0));
+            else if (al.get(i).equals("SENDER") && i + 1 < al.size())
+                _sender = AddressType.getAddressType(al.get(i + 1).charAt(0));
+            else if (al.get(i).equals("DATA") && i + 1 < al.size()) setContent(al.get(i + 1));
         }
-        packetType = PType.getPT(buffer.charAt(0));
-        buffer.deleteCharAt(0);
-        return buffer.toString();
+
     }
 }
