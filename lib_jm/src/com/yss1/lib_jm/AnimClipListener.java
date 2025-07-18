56e64e2fa463735e1623d72e39f054a8f914b5e0
yss120612 <yss1@yandex.ru>
2025-07-18 18:28:40 +0800

    180725test added

--- lib_jm/src/com/yss1/lib_jm/AnimClipListener.java
+++
@@ -1,9 +0,0 @@
-package com.yss1.lib_jm;
-
-import com.jme3.anim.AnimComposer;
-import com.jme3.anim.tween.action.Action;
-
-//Ловит событие окончания анимации
-public interface AnimClipListener {
-    public void onAnimCycleDone(Action action, AnimComposer animComposer, String animName);
-}
